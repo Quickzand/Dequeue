b74dbf613f03dd8743d512f45f8d01a51bfe7079
Matthew Sand <matthewsand22@gmail.com>
2021-07-23 15:44:52 -0400

    Worked on the landing page and a redesign concept

--- appRedesign/js/main.js
+++ appRedesign/js/main.js
@@ -1,17 +1,5 @@
-function toggleNavGroup() {
-  $(".smallNavButtonGroup").toggleClass("open").toggleClass("closed")
-  console.log("test")
+// Toggles open / close class on parent of element
+function toggleSmallNavButtonGroup(element) {
+  $(element).parent().toggleClass("open").toggleClass("closed")
 }
 
-// Sends request post request to server
-function sendRequest(input) {
-  var request = new XMLHttpRequest();
-  request.open("POST", "http://localhost:3000/api/v1/user", true);
-  request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-  request.onreadystatechange = function() {
-    if (request.readyState == 4 && request.status == 200) {
-      console.log(request.responseText);
-    }
-  };
-  request.send(input);
-}
